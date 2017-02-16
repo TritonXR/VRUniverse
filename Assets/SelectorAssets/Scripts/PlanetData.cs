a4f0aaf8a80871fb15bcacfbac3308f704bf9377
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-02-15 16:23:46 -0800

    Organized work. Error in selecting objects

--- Assets/SelectorAssets/Scripts/PlanetData.cs
+++ Assets/SelectorAssets/Scripts/PlanetData.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class controller_menu : MonoBehaviour {
+public class PlanetData : MonoBehaviour {
+
+	public string planet_name, year, description;
 
 	// Use this for initialization
 	void Start () {
 	
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+
 	}
 }
