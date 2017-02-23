2787259243b09f673d8a56513570e9550db3c3d6
allenlinxp <864414788@qq.com>
2017-02-22 17:56:10 -0800

    complete_version

--- Assets/SelectorAssets/Scripts/Controller.cs
+++ Assets/SelectorAssets/Scripts/Controller.cs
@@ -7,32 +7,31 @@ public class Controller : VRTK_InteractableObject {
 
 	public Text panel;
 
-    public GameObject planet1;
-	public PlanetData planet1_script;
+	private PlanetData planet_script;
+
+    public GameObject UsingObject;
 
 
 	// Use this for initialization
 	void Start () {
-        //Debug.Log("Started");
-		planet1_script = planet1.GetComponent<PlanetData> ();
-
 	}
 
     public override void StartUsing(GameObject currentUsingObject)
     {
         base.StartUsing(currentUsingObject);
-        Debug.Log("Name: " + planet1_script.planet_name);
-        //panel.text = "Name:" + planet1_script.planet_name + "\n"
-        //        + "Year: " + planet1_script.year + "\n"
-        //        + "Description: " + planet1_script.description;
-        //Debug.Log("Using: text is: " + panel.text);
+        UsingObject = currentUsingObject;
+        planet_script = gameObject.GetComponent<PlanetData>();
+        panel.text = "Name:" + planet_script.planet_name + "\n"
+                + "Year: " + planet_script.year + "\n"
+                + "Description: " + planet_script.description;
+
     }
 
     public override void StopUsing(GameObject previousUsingObject)
     {
-        //Debug.Log("Stop using");
         base.StopUsing(previousUsingObject);
         panel.text = "";
+        StartUsing(UsingObject);
     }
     // Update is called once per frame
     protected override void Update () {
