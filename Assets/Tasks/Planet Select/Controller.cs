5219cde24bd815409f8b5b8dabae507733f91a03
allenlinxp <864414788@qq.com>
2017-02-25 17:29:57 -0800

    Attempted to test in Vive after merging. Doesn't work yet.

--- Assets/Tasks/Planet Select/Controller.cs
+++ Assets/Tasks/Planet Select/Controller.cs
@@ -8,7 +8,7 @@ public class Controller : VRTK_InteractableObject
 
     public Text Title, Creator, Description, Year, Tag;
 
-    public Image iamgeDes;
+    public Image imageDes;
 
     private PlanetData planet_script;
 
@@ -22,19 +22,29 @@ public class Controller : VRTK_InteractableObject
 
     public override void StartUsing(GameObject currentUsingObject)
     {
+        Debug.Log("Planet_seclected");
         base.StartUsing(currentUsingObject);
         UsingObject = currentUsingObject;
         planet_script = gameObject.GetComponent<PlanetData>();
+
+        Debug.Log("start getting info");
         Title.text = planet_script.title;
         Creator.text = planet_script.creator;
         Description.text = planet_script.description;
         Year.text = planet_script.year;
         Tag.text = planet_script.des_tag;
 
+        imageDes.sprite = planet_script.image;
+
+        Debug.Log("completed");
+
+
     }
 
     public override void StopUsing(GameObject previousUsingObject)
     {
+        Debug.Log("stop");
+
         base.StopUsing(previousUsingObject);
         StartUsing(UsingObject);
     }
