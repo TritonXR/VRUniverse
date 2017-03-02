3d3a68f798bf26da74bf510509dc6eb003dcc12b
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-03-01 18:46:31 -0800

    Can use trigger to select planet. And teleport with touchpad. Fixed merge.

--- Assets/Tasks/Planet Select/Controller.cs
+++ Assets/Tasks/Planet Select/Controller.cs
@@ -22,12 +22,19 @@ public class Controller : VRTK_InteractableObject
 
     public override void StartUsing(GameObject currentUsingObject)
     {
-        Debug.Log("Planet_seclected");
+        //Debug.Log("1name of selected project" + gameObject.name);
+
+        //currentUsingObject.GetComponent<VRTK_SimplePointer>().enableTeleport = false;
+
+        //Debug.Log("setting the : " + currentUsingObject.name + " equal to false");
         base.StartUsing(currentUsingObject);
+
+        //currentUsingObject.GetComponent<VRTK_SimplePointer>().enableTeleport = false;
+
         UsingObject = currentUsingObject;
         planet_script = gameObject.GetComponent<PlanetData>();
 
-        Debug.Log("start getting info");
+        //Debug.Log("2name of selected project" + gameObject.name);
         Title.text = planet_script.title;
         Creator.text = planet_script.creator;
         Description.text = planet_script.description;
@@ -36,7 +43,7 @@ public class Controller : VRTK_InteractableObject
 
         imageDes.sprite = planet_script.image;
 
-        Debug.Log("completed");
+        //Debug.Log("3name of selected project" + gameObject.name);
 
 
     }
@@ -44,8 +51,11 @@ public class Controller : VRTK_InteractableObject
     public override void StopUsing(GameObject previousUsingObject)
     {
         Debug.Log("stop");
-
+        //previousUsingObject.GetComponent<VRTK_SimplePointer>().enableTeleport = false;
         base.StopUsing(previousUsingObject);
+
+        //previousUsingObject.GetComponent<VRTK_SimplePointer>().enableTeleport = false;
+
         StartUsing(UsingObject);
     }
     // Update is called once per frame
