937906be0ffabeaf00d2a3557743a27b3b450015
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-03-04 18:14:23 -0800

    lever works on main scene!

--- Assets/Tasks/Pull/Lever.cs
+++ Assets/Tasks/Pull/Lever.cs
@@ -1,31 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using VRTK;
 
-public class Lever : MonoBehaviour {
+public class Lever : VRTK_InteractableObject {
 
 	bool leverOn = false;
 	private Animator anima;
 
-	void OnTriggerStay(Collider other) {
-		if (other.tag == "Player" )
-		{
-			Debug.Log("is player");
-		    if (Input.GetMouseButtonDown(0))
-			{
-				Debug.Log("left clicked");
-				anima.SetBool("switch", true);
-				anima.SetBool("switch 2", false);
+    private bool displayInfo;
 
-				leverOn = true;
-			}
-			else if (Input.GetMouseButtonDown(1))
-			{
-				Debug.Log("right clicked");
-				anima.SetBool("switch", false);
-				anima.SetBool("switch 2", true);
-				leverOn = false;
-			}	
-		}
+
+    public override void StartUsing(GameObject usingObject)
+    {
+        base.StartUsing(usingObject);
+        displayInfo = true;
+        Debug.Log("StartUsing");
+    }
+
+    public override void StopUsing(GameObject usingObject)
+    {
+        base.StopUsing(usingObject);
+        displayInfo = false;
+        Debug.Log("StopUsing");
+    }
+
+
+    void OnTriggerStay(Collider other) {
+        if (other.name == "Body") {
+            Debug.Log("is player");
+            if (displayInfo)
+            {
+                Debug.Log("left clicked");
+                anima.SetBool("switch", true);
+                anima.SetBool("switch 2", false);
+
+                leverOn = true;
+            }
+            else
+            {
+                Debug.Log("right clicked");
+                anima.SetBool("switch", false);
+                anima.SetBool("switch 2", true);
+                leverOn = false;
+            }
+        }
+		
 	}
 
 	// Use this for initialization
@@ -36,10 +55,9 @@ public class Lever : MonoBehaviour {
 
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		
-	}
+
+    protected override void Update()
+    {
+        base.Update();
+    }
 }
