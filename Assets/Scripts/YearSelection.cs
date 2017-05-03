d16c715da3f543e279c69b51c1d694cbaf1accba
Cortez Page <cvpage@ucsd.edu>
2017-05-03 12:47:03 -0700

    Year Selection

---
+++ Assets/Scripts/YearSelection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YearSelection : MonoBehaviour
+{
+	private static readonly int CURRENT_YEAR = DateTime.Now.Year;
+	private int selectedYear = CURRENT_YEAR;
+	private int displayedYear;
+	private Text yearText;
+	
+	private void Start()
+	{
+		displayedYear = selectedYear;
+		yearText = GetComponent<Text>();
+		updateYearText();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A) && displayedYear != selectedYear)
+		{
+			selectedYear = displayedYear;
+			updateYearText();
+			Debug.Log("Traveling to year " + selectedYear);
+		}
+	}
+
+	public void nextYear()
+	{
+		displayedYear++;
+		updateYearText();
+	}
+
+	public void prevYear()
+	{
+		displayedYear--;
+		updateYearText();
+	}
+
+	private void updateYearText()
+	{
+		yearText.text = displayedYear.ToString();
+		yearText.color = displayedYear == selectedYear ? Color.green : Color.black;
+	}
+}
