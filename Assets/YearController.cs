eebedab8d8d0ade4ad2d6b6eb7bb4fd80ead6dc0
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-05-25 10:21:18 -0700

    Fixed null reference. But radial menu is gone

--- Assets/YearController.cs
+++ Assets/YearController.cs
@@ -9,8 +9,23 @@ public class YearController : MonoBehaviour
     private void Start ()
     {
         yearSelection = Camera.main.transform.root.GetComponentInChildren<YearSelection>();
-        SteamVR_TrackedController leftController = Camera.main.transform.root.GetComponentInChildren<SteamVR_TrackedController>();
-        leftController.TriggerClicked += HandleTriggerClicked;
+        SteamVR_TrackedController[] leftController = Camera.main.transform.root.GetComponentsInChildren<SteamVR_TrackedController>();
+        if (Camera.main)
+        {
+            Debug.Log("camera exists");
+        } else
+        {
+            Debug.Log("camera not exists");
+        }
+        Debug.Log("The root is: " + Camera.main.transform.root.name);
+        for (int i = 0; i < leftController.Length; i++)
+        {
+            if (leftController[i].GetComponentInChildren<YearSelection>())
+            {
+                leftController[i].TriggerClicked += HandleTriggerClicked;
+            }
+        }
+        
     }
 
     private void HandleTriggerClicked(object sender, ClickedEventArgs e)
