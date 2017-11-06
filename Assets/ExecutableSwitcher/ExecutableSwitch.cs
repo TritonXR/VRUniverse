452948ecbb7b654ea9daa1a17128d19ef4441e39
Dana Kimball <dkimball@ucsd.edu>
2017-11-05 16:45:33 -0800

    Largely functional but unstable switcher working

--- Assets/ExecutableSwitcher/ExecutableSwitch.cs
+++ Assets/ExecutableSwitcher/ExecutableSwitch.cs
@@ -7,13 +7,11 @@ public class ExecutableSwitch : MonoBehaviour
 
 {
     private static ExecutableSwitch switcher = null;
-    private SteamVR_TrackedController controller_left = null;
-    private SteamVR_TrackedController controller_right = null;
 
-    public SteamVR_LoadLevel loadlevelscript;
-    public string defaultDatapath = "/../../../../../VRUniverse.exe";
-    public bool exitByMenuButton = false;
-    public float delayedSearchTime = 0.75f;
+    public string defaultDatapath = "VRUniverse.exe";
+
+    private char[] delimiterList = { '/', '\\', '.' };
+    private bool isLoading;
 
     private void Awake()
     {
@@ -22,125 +20,39 @@ public class ExecutableSwitch : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        isLoading = false;
 
         DontDestroyOnLoad(gameObject);
-    }
-
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += HandleLevelLoaded;
-        SetupControllerListener();
-        if (controller_left == null || controller_right == null) StartCoroutine(DelayedSetup());
-    }
-
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= HandleLevelLoaded;
-        CleanUpControllerListener();
     }
 
     void OnDestroy()
     {
         if(switcher == this) switcher = null;
-        CleanUpControllerListener();
     }
 
     public void LoadExecutable(string datapath)
     {
-        if (loadlevelscript == null) return;
+        string[] splitDatapath = datapath.Split(delimiterList);
+        string processName = splitDatapath[splitDatapath.Length - 2]; //should extract the process name
+
+        LoadExecutable(datapath, processName);
+    }
+
+    public void LoadExecutable(string datapath, string processName)
+    {
+        if (isLoading) return;
+        else isLoading = true;
         if (datapath == null || datapath.Equals("")) datapath = defaultDatapath;
-        if (!datapath.StartsWith("/") && !datapath.StartsWith("\\")) datapath = "/" + datapath;
 
-        loadlevelscript.levelName = datapath;
+        System.Diagnostics.Process childProcess = System.Diagnostics.Process.Start(Application.dataPath + datapath);
+        System.Diagnostics.Process rebootProcess = System.Diagnostics.Process.Start(Application.dataPath + "/../ProcessRebooter.exe", "VRUniverse.exe " + processName);
 
-        loadlevelscript.internalProcessPath = Application.dataPath + "/" + datapath;
-
-        loadlevelscript.Trigger();
+        Application.Quit();
     }
 
     public void LoadExecutable()
     {
         LoadExecutable(defaultDatapath);
-    }
-
-    private void SetupControllerListener()
-    {
-        CleanUpControllerListener();
-        GameObject l_control = GameObject.Find("[CameraRig]/Controller (left)");
-        if (l_control != null)
-        {
-            controller_left = l_control.GetComponent<SteamVR_TrackedController>();
-            if (controller_left == null)
-            {
-                Debug.LogWarning("Could not find left SteamVR_TrackedController script. Cannot detect exit button press.");
-            }
-        }
-        else
-        {
-            Debug.LogWarning("Could not find left controller. Cannot detect exit button press.");
-            controller_left = null;
-        }
-
-        GameObject r_control = GameObject.Find("[CameraRig]/Controller (right)");
-        if (r_control != null)
-        {
-            controller_right = l_control.GetComponent<SteamVR_TrackedController>();
-            if (controller_right == null)
-            {
-                Debug.LogWarning("Could not find right SteamVR_TrackedController script. Cannot detect exit button press.");
-            }
-        }
-        else
-        {
-            Debug.LogWarning("Could not find right controller. Cannot detect exit button press.");
-            controller_right = null;
-        }
-
-        if(controller_left != null || controller_right != null)
-        {
-            if(controller_left != null)
-            {
-                controller_left.MenuButtonClicked += HandleMenuClicked;
-            } else
-            {
-                Debug.LogWarning("Could not register left controller.");
-            }
-
-            if (controller_right != null)
-            {
-                controller_right.MenuButtonClicked += HandleMenuClicked;
-            } else
-            {
-                Debug.LogWarning("Could not register left controller.");
-            }
-        }
-    }
-
-    private void CleanUpControllerListener()
-    {
-        if(controller_left != null)
-        {
-            controller_left.MenuButtonClicked -= HandleMenuClicked;
-            controller_left = null;
-        }
-
-        if(controller_right != null)
-        {
-            controller_right.MenuButtonClicked -= HandleMenuClicked;
-            controller_right = null;
-        }
-    }
-
-    private void HandleLevelLoaded(Scene scene, LoadSceneMode mode)
-    {
-        SetupControllerListener();
-        if (controller_left == null || controller_right == null) StartCoroutine(DelayedSetup());
-    }
-
-    private void HandleMenuClicked(object sender, ClickedEventArgs e)
-    {
-        Debug.Log("Detected Menu Button Click");
-        if (exitByMenuButton) LoadExecutable();
     }
 
     public static ExecutableSwitch GetExecutableSwitch()
@@ -159,11 +71,5 @@ public class ExecutableSwitch : MonoBehaviour
     {
         if (switcher == null) Debug.LogError("ExecutableSwitcher Error: No ExecutableSwitch script exists in this scene!");
         else switcher.LoadExecutable(datapath);
-    }
-
-    IEnumerator DelayedSetup()
-    {
-        yield return new WaitForSeconds(delayedSearchTime);
-        SetupControllerListener();
     }
 }
