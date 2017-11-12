d7faa9c6566dd6b1b7bc65946bef796ce43941f9
Subhankar Panda <s1panda@ucsd.edu>
2017-11-12 15:38:10 -0800

    v v v v  vv  v vv  vv rough start to the projects page and added json field

--- Website/app.js
+++ Website/app.js
@@ -24,7 +24,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use("/data", express.static(path.join(__dirname, 'data')));
 
 app.use('/', index);
 app.use('/about', about);
