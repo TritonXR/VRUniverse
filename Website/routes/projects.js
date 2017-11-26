622857d79b9cf628c7a25cb88c0638dd59e0e933
Subhankar Panda <s1panda@ucsd.edu>
2017-11-25 18:59:42 -0800

    removed a bunch of data to push

--- Website/routes/projects.js
+++ Website/routes/projects.js
@@ -2,36 +2,29 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs");
 
-
-
 function readFiles(dirname, callback) {
     var data = {};
     var counter = 0;
     var sum;
     fs.readdir(dirname, function (err, filenames) {
-        console.log("doing read dir");
         if (err) {
             console.log(err);
             return;
         }
-        console.log(typeof filenames);
         var array = filenames
             .filter(function (filename) {
                 return filename.substr(-5) === '.json';
             });
         sum = array.length;
-       array.forEach(function (filename) {
-                console.log(dirname + filename);
+        array.forEach(function (filename) {
                 fs.readFile(dirname + filename, 'utf-8', function (err, content) {
                     if (err) {
                         onError(err);
                         return;
                     }
-                    console.log("doing read file");
                     var year = filename.split(".")[0];
                     data[year] = content;
                     counter++;
-                    console.log("counter is " + counter);
                     if (counter === sum) {
                         callback(data)
                     }    
@@ -41,11 +34,6 @@ function readFiles(dirname, callback) {
     });
     
 } 
-
-
-
-/* GET users listing. */
-
 
 router.get('/', function (req, res, next) {
     
@@ -57,10 +45,4 @@ router.get('/', function (req, res, next) {
     });
 });
 
-
-  
-
-
-
-//{ "data" : JSON.stringify(data) }
 module.exports = router;
