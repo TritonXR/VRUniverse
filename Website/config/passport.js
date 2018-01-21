8e95ee6715da8a24456658835baa2fd3873a6ffd
Lamy1135 <oscarikiki@gmail.com>
2018-01-20 17:53:25 -0800

    signin/signup/passport/mongodb/etc
    please run npm install first

---
+++ Website/config/passport.js
@@ -0,0 +1,57 @@
+var passport = require('passport');
+var User = require('../model/user');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use('local.signup', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'passport',
+    passReqToCallback: true
+}, function(req, username, password, done) {
+    User.findOne({'username': username}, function(err, user) {
+        if(err) {
+            return done(err);
+        }
+        if(user) {
+            return done(null, false, {message: 'user already existed!'});
+        }
+        var newUser = new User();
+        newUser.username = username;
+        newUser.password = password;
+        newUser.save(function(err, result) {
+            if(err) {
+                return done(err);
+            }
+            return done(null, newUser);
+        });
+
+    });
+}));
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'passport',
+    passReqToCallback: true
+}, function(req, uesrname, password, done) {
+    User.findOne({'username': username}, function(err, user) {
+        if(err) {
+            return done(err);
+        }
+        if(!user) {
+            return done(null, false, {message: 'failed'});
+        }
+        if(!user.validPassword(password)) {
+            return done(null, false, {message: 'failed'});
+        }
+        return done(null, user);
+    });
+}));
--- Website/routes/index.js
+++ Website/routes/index.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'VR Club' });
 
 });
+
+router.get('/signup', function(req, res, next) {
+    var messages = req.flash('error');
+    res.render('/signup', {
+        csrfToken: req.csrfToken(),
+        messages: messages,
+        hasErrors: messages.length > 0
+    });
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+
+router.get('/signin', function(req, res, next) {
+    var messages = req.flash('error');
+    res.render('/signin', {
+        csrfToken: req.csrfToken(),
+        messages: messages,
+        hasErrors: messages.length > 0
+    });
+});
+
+router.post('/signin', passport.authenticate('local.signin', {
+    successRedirect: '/upload',
+    failureRedirect: '/signin',
+    failureFlash: true
+}));
+
 
 module.exports = router;
