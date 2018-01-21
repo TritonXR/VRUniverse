6f354878743334f3da6fe27cce2b109b1cf56f4b
Dana Kimball <dkimball@ucsd.edu>
2018-01-20 18:44:43 -0800

    Nearly Finished Prototype

--- Assets/Scripts/OrbitManager.cs
+++ Assets/Scripts/OrbitManager.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class OrbitManager : MonoBehaviour {
 
-	public float OrbitRadius = 500.0f;
-	public float PlanetDist = 200.0f;
+	private const float FULL_CIRCLE = 2.0f * Mathf.PI;
+
+	public float OrbitRadiusScaling = 10.0f;
+	public float OrbitRadiusBase = 300.0f;
+	public float PlanetDist = 150.0f;
 	public float AngleCeiling = 60.0f;
 	public float AngleFloor = -10.0f;
+	public float LinearSpeed = 20.0f;
 
 	public int num_planets = 20;
 	public GameObject prefab;
+	public Light StarLight;
+	public Camera MainCamera;
 
-	public float angularSpeed = 6.0f; //degrees per second
-
-	public float polarLocation;
+	private float angularSpeed;
+	private float polarLocation;
+	private float orbitRadius;
 
 	// Use this for initialization
 	void Start () {
 
-		float theta = (2 * Mathf.PI) / num_planets;
+		float theta = FULL_CIRCLE / num_planets;
+		orbitRadius = OrbitRadiusScaling * num_planets + OrbitRadiusBase;
+		angularSpeed = LinearSpeed / orbitRadius;
+		StarLight.range = 1.2f * (orbitRadius + PlanetDist);
+		MainCamera.farClipPlane = 2.1f * (orbitRadius + PlanetDist);
 
 		for (int count = 0; count < num_planets; count++)
 		{
 			float epsilon = Random.Range(AngleFloor, AngleCeiling);
 
-			float radius = OrbitRadius + (1.0f - 2.0f * (count % 2)) * PlanetDist * Mathf.Cos(epsilon * Mathf.Deg2Rad);
+			float radius = orbitRadius + (1.0f - 2.0f * (count % 2)) * PlanetDist * Mathf.Cos(epsilon * Mathf.Deg2Rad);
 			float elevation = PlanetDist * Mathf.Sin(epsilon * Mathf.Deg2Rad);
 
 			Vector3 position = new Vector3(radius * Mathf.Cos(theta * count), elevation, radius * Mathf.Sin(theta * count));
@@ -40,9 +50,10 @@ public class OrbitManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		polarLocation += angularSpeed * Time.deltaTime;
+		while (polarLocation > FULL_CIRCLE) polarLocation -= FULL_CIRCLE;
 
-		Vector3 position = new Vector3(OrbitRadius * Mathf.Cos(polarLocation * Mathf.Deg2Rad), transform.position.y, OrbitRadius * Mathf.Sin(polarLocation * Mathf.Deg2Rad));
+		Vector3 position = new Vector3(orbitRadius * Mathf.Cos(polarLocation), transform.position.y, orbitRadius * Mathf.Sin(polarLocation));
 		transform.position = position;
-		transform.eulerAngles = new Vector3(0, 180 - polarLocation, 0);
+		transform.eulerAngles = new Vector3(0, 180.0f - (polarLocation * Mathf.Rad2Deg), 0);
 	}
 }
