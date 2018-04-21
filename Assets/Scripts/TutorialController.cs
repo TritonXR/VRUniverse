7d5d27234d38a3242a5cf39cd74e60bf860a0b05
Kristin Agcaoili <kagcaoil@ucsd.edu>
2018-04-21 16:56:38 -0700

    Started implementing backend for tutorial

---
+++ Assets/Scripts/TutorialController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject welcomeTutorial_1; // headset. disappear when specific action is completed
+    [SerializeField]
+    private GameObject yearTutorial_2;
+    [SerializeField]
+    private GameObject categoryTutorial_3;
+    [SerializeField]
+    private GameObject searchTutorial_4;
+    [SerializeField]
+    private GameObject exit_5; // be there forever
+    [SerializeField]
+    private GameObject lever_6; // be there forever
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
