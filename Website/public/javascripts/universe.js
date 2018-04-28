dafbd757aa7a48d5f44b6b1dc7c5ad9f783898c9
Subhankar Panda <s1panda@ucsd.edu>
2018-04-28 14:18:20 -0700

    New Navbar

--- Website/public/javascripts/universe.js
+++ Website/public/javascripts/universe.js
@@ -1,2 +1,8 @@
 $(document).ready(function() {
+  function classToggle() { 
+    const navs = document.querySelectorAll('.u-nav__items')
+    navs.forEach(nav => nav.classList.toggle('u-nav__link-toggle-show'));
+  }
+  document.querySelector('.u-nav__link-toggle')
+    .addEventListener('click', classToggle);
 });
