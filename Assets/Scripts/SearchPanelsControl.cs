8e6d8819f338eb2fc6d646928fef736aac2061a7
faraway2018 <hcmhcm426@gmail.com>
2018-05-19 14:45:39 -0700

    Linked controller

--- Assets/Scripts/SearchPanelsControl.cs
+++ Assets/Scripts/SearchPanelsControl.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SearchPanelsControl : MonoBehaviour {
 
     // new panels = UniverseSystem.GetInstance().GetComponent<Canvas>();
     private Canvas[] newpanels;
     private bool turnup;
+    [SerializeField] private Text displaytext;
     // private ToggleButton 
 
     // Use this for initialization
@@ -14,11 +16,95 @@ public class SearchPanelsControl : MonoBehaviour {
         newpanels = UniverseSystem.GetInstance().panels;
         turnup = false;
         clearPanels();  //will that be a delay  
+        displaytext.text = "Display: Off";
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.C))
+        /*if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (turnup == false)
+            {
+                displayPanels();
+                turnup = true;
+            }
+            else
+            {
+                clearPanels();
+                turnup = false;
+            }
+        }*/
+        SetLabel(turnup);
+    }
+
+    public void displayPanels()
+    {
+        //SearchResultsCanvas --> ResultDisplay GetComponentsInChildren<Box Collider>
+        //CategoiresCanvas --> CategoryManager
+        for (int index = 0; index < newpanels.Length; index++)
+        {
+            if (newpanels[index].GetComponent<CategoryManager>() != null) //access PanelLeft
+            {
+                newpanels[index].enabled = true;
+                BoxCollider[] SearchResultArray = newpanels[index].GetComponentsInChildren<BoxCollider>();
+                for (int i = 0; i < SearchResultArray.Length; i++)
+                {
+                    SearchResultArray[i].enabled = true;
+                }
+
+            }
+            else if (newpanels[index].GetComponentInChildren<ResultDisplay>() != null)
+            {
+                newpanels[index].enabled = true;
+                BoxCollider[] CategoriesArray = newpanels[index].GetComponentsInChildren<BoxCollider>();
+                for (int i = 0; i < CategoriesArray.Length; i++)
+                {
+                    CategoriesArray[i].enabled = true;
+                }
+            }
+            else
+            {
+                continue;
+            }
+
+
+        }
+    }
+
+    public void clearPanels()
+    {
+        for (int index = 0; index < newpanels.Length; index++)
+        {
+            if (newpanels[index].GetComponent<CategoryManager>() != null) //access PanelLeft
+            {
+                newpanels[index].enabled = false;
+                BoxCollider[] SearchResultArray = newpanels[index].GetComponentsInChildren<BoxCollider>();
+                for (int i = 0; i < SearchResultArray.Length; i++)
+                {
+                    SearchResultArray[i].enabled = false;
+                }
+
+            }
+            else if (newpanels[index].GetComponentInChildren<ResultDisplay>() != null)
+            {
+                newpanels[index].enabled = false;
+                BoxCollider[] CategoriesArray = newpanels[index].GetComponentsInChildren<BoxCollider>();
+                for (int i = 0; i < CategoriesArray.Length; i++)
+                {
+                    CategoriesArray[i].enabled = false;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+
+    //attach the button into action
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("GameController"))
         {
             if (turnup == false)
             {
@@ -33,25 +119,16 @@ public class SearchPanelsControl : MonoBehaviour {
         }
     }
 
-    public void displayPanels()
-    {
-        for (int index = 0; index < newpanels.Length; index++)
-        {
-            if (newpanels[index].GetComponent<CategoryManager>() != null || newpanels[index].GetComponentInChildren<ResultDisplay>() != null) //access PanelLeft
-            {
-                newpanels[index].enabled = true;
-            }
-        }
-    }
 
-    public void clearPanels()
+    private void SetLabel(bool turnup)
     {
-        for (int index = 0; index < newpanels.Length; index++)
+        if (turnup == false)
         {
-            if (newpanels[index].GetComponent<CategoryManager>() != null || newpanels[index].GetComponentInChildren<ResultDisplay>() != null) //access PanelLeft
-            {
-                newpanels[index].enabled = false;
-            }
+            displaytext.text = "Display: Off";
+        }
+        else
+        {
+            displaytext.text = "Display: On";
         }
     }
 
